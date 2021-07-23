19953554e06a3110b9463ccb67b22da651518d2d
Matheus Martins de carvalho <matheus.carvalho@mobilemed.com.br>
2021-07-23 17:28:49 -0300

    fix cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { ContactRouter } from './src/routes/contacts'
+import {cors} from 'cors'
 
 const port = 3000
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 const contactRouter = new ContactRouter()
 
